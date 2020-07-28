b232601ed7aa423fb34649f4e62327b4f99fd245
yuriyanagi <40306yy@gmail.com>
2020-07-28 15:29:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'records/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  root "records#index"
   resources :records, only: :index
 
 end
