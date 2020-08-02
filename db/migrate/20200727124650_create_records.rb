da531cf64e03cb7e4d17028587812ec0f6087bcd
yuriyanagi <40306yy@gmail.com>
2020-08-03 00:45:13 +0900

    Update 20200727124650_create_records.rb

--- db/migrate/20200727124650_create_records.rb
+++ db/migrate/20200727124650_create_records.rb
@@ -1,9 +1,9 @@
 class CreateRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :records do |t|
-      t.date :date, null: false
-      t.time :start_time, null: false
-      t.time :end_time, null: false
+      t.date :date
+      t.time :start_time
+      t.time :end_time
       t.string :major_class, null: false
       t.string :middle_class, null: false
       t.string :small_class, null: false
